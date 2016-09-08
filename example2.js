c1c254222ef31a66ae15aa3c89a9b9439d4bc7f1
Angelo Bovino <angelo.bovino@outlook.com>
2016-09-07 20:17:16 -0400

    2nd example multiple divs

---
+++ example2.js
@@ -0,0 +1,12 @@
+//Include the main react dependencies
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+
+  <div>
+
+
+    
+  </div>
+  , document.getElementById('app'));
